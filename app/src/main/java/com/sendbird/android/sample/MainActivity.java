291fbf6ce52cbbfb9d92c3fc38c2a641ea83e5ef
Dominic <dkirby@umail.ucsb.edu>
2017-03-13 22:06:04 -0700

    //here

--- app/src/main/java/com/sendbird/android/sample/MainActivity.java
+++ app/src/main/java/com/sendbird/android/sample/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends FragmentActivity {
      * Also you need to set Server API Token and Sender ID in SendBird dashboard.
      * Please carefully read "Push notifications" section in SendBird Android documentation
      */
+    //here
     private static final String appId = "9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23"; /* Sample SendBird Application */
 
     public static String sUserId;
